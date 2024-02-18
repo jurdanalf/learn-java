7a5f5a55af211431e7adb2633db54aa1ba8069ec
jurdanalf <jurdanalfirdauz@gmail.com>
2024-02-18 21:47:11 +0700

    learning loop

--- JavaDasar/src/Loop.java
+++ JavaDasar/src/Loop.java
@@ -1,7 +1,13 @@
 public class Loop {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println("Hahahahaha");
+            if (i % 2 == 1) {
+                System.out.println( i + " adalah bilangan Prima");
+            }
+        }
+
+        for (int s = 1; s <= 10; s++) {
+            System.out.print("o ");
         }
     }
 }
