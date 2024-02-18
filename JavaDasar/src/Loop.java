3f17a633ee74badef7d4320684f4682a4dfc4cdf
jurdanalf <jurdanalfirdauz@gmail.com>
2024-02-18 22:08:52 +0700

    learning array multi dimensi

--- JavaDasar/src/Loop.java
+++ JavaDasar/src/Loop.java
@@ -1,5 +1,6 @@
 public class Loop {
     public static void main(String[] args) {
+        // looping for
         for (int i = 1; i < 10; i++) {
             if (i % 2 == 1) {
                 System.out.println( i + " adalah bilangan Prima");
@@ -8,6 +9,13 @@ public class Loop {
 
         for (int s = 1; s <= 10; s++) {
             System.out.print("o ");
+        }
+        System.out.println("\n");
+
+        // looping dengan foreach
+        String[] cars = {"Honda","Toyota","Hyundai","Wuling"};
+        for (String car : cars) {
+            System.out.println(car);
         }
     }
 }
